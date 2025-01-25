df2253996922256058f42c9a3eb2bac7c18ba661
WashingtonDDS <washington.DDS@hotmail.com>
2025-01-25 20:58:29 -0300

    add form validation for transaction inputs in Home component

---
+++ src/interfaces/homeInterface.ts
@@ -0,0 +1,4 @@
+export type TTransaction = {
+  description: string;
+  price: number;
+};
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,39 @@
 import styles from "./home.module.css";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { TTransaction } from "../../interfaces/homeInterface";
+
+const schema = yup
+  .object({
+    description: yup.string().required("A descrição é obrigatória"),
+    price: yup
+      .number()
+      .transform((value) => (Number.isNaN(value) ? null : value))
+      .required("O preço é obrigatorio"),
+  })
+  .required();
 export function Home() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const addTransaction = (inputsValue: TTransaction) => {
+    console.log(inputsValue);
+  };
+
   return (
     <div className={styles.container__home}>
       <div className={styles.content__home}>
-        <form>
+        <form onSubmit={handleSubmit(addTransaction)}>
           <h2>Contas</h2>
-          <input name="description" type="text" placeholder="Conta" />
+          <input {...register("description")} type="text" placeholder="Conta" />
 
-          <input name="price" type="number" placeholder="Preço R$" />
+          <input {...register("price")} type="number" placeholder="Preço R$" />
 
           <button className={`btn btn__positive ${styles.btn__register}`}>
             CADASTRAR
