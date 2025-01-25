2533f04cba2937560259c1daee9d9bac5e7027e5
WashingtonDDS <washington.DDS@hotmail.com>
2025-01-24 22:16:43 -0300

    refactor SignIn component to integrate form validation using react-hook-form and yup

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,22 +1,43 @@
-import styles from './signIn.module.css'
+import styles from "./signIn.module.css";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { TInputsValue } from "../../interfaces/signUpInterface";
+import api from "../../services/api";
+import { useNavigate } from "react-router-dom";
+
+const schema = yup
+  .object({
+    name: yup.string().required("O nome é obrigatorio"),
+    email: yup
+      .string()
+      .required("O email é obrigatorio")
+      .email("Email invalido"),
+    password: yup
+      .string()
+      .required("A senha é obrigatorio")
+      .min(8, "A senha precisa ter pelo menos 8 caracteres"),
+  })
+  .required();
+
 export function SignIn() {
   return (
     <main className={styles.main}>
-      <div className={styles.containerLeft}>
-      </div>
+      <div className={styles.containerLeft}></div>
       <div className={styles.containerRight}>
         <form className={styles.containerRight__form}>
-
-
           <span>Bem vindo</span>
-          <h1 className={styles.containerRight__title}>Faça o login com sua conta</h1>
+          <h1 className={styles.containerRight__title}>
+            Faça o login com sua conta
+          </h1>
 
           <input name="email" type="text" placeholder="Email" />
 
           <input name="password" type="password" placeholder="Senha" />
 
-          <button className={`btn btn__positive ${styles.btn__login}`}>LOGIN</button>
-
+          <button className={`btn btn__positive ${styles.btn__login}`}>
+            LOGIN
+          </button>
         </form>
 
         <div className={styles.register__link}>
@@ -24,8 +45,6 @@ export function SignIn() {
           <a href="#">Clique aqui!</a>
         </div>
       </div>
-
     </main>
-
-  )
+  );
 }
